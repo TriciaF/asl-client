d5d21d05ea69a0f227146a999bde4bdfa2595d7f
TriciaF <TriciaF@thinkful.com>
2018-04-01 17:09:09 -0400

    modified the CLIENT ORIGIN to point to netlify URL

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://asl-learning.herokuapp.com/http/api';
-export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://asl-learning.netlify.com';
 export const PORT = process.env.PORT || 8080;
