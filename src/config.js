f1ebdeb150a006310073490687807a91c4fabb04
TriciaF <TriciaF@thinkful.com>
2018-04-02 09:30:45 -0400

    modied the API_BASE_URL

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://learn-asl-alphabet.herokuapp.com/api';
-export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://learn-asl-alphabet.netlify.com';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://learning-asl-alphabet.herokuapp.com/api';
+export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://learning-asl-alphabet.netlify.com';
 export const PORT = process.env.PORT || 8080;
